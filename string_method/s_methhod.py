21850a3f42afcab7ba9b76614db4ab15e1953b95
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-20 22:30:46 +0100

    Add files via upload

---
+++ string_method/s_methhod.py
@@ -0,0 +1,16 @@
+name = 'elijah'
+print(len(name))
+# the len function is used to count numbers of items in a string
+age = 'twenty'
+print(age.upper())
+# the functions that are particular to a string are called methods and to initialize them type the value and add a dot(.) to it
+# for replace
+self = 'Body'
+print(self.replace('B','G'))
+
+# TO FIND THE index
+name = 'Tochi'
+print(name.find('chi'))
+# checking the existence of chara ters in an operator
+name = 'Elijah'
+print('E' in name)
